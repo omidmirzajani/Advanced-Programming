6ad965fa70222959fb82d212fb71ab5498deeef1
omidmirzajani <omidmirzajani82@gmail.com>
2020-03-27 16:49:57 +0430

    A14 Added

---
+++ A14/Code/A14/A14/PointState.cs
@@ -0,0 +1,24 @@
+﻿namespace A14
+{
+    /// <summary>
+    /// این وضعیت نشان دهنده حالتی است که نقطه زده شده
+    /// این وضعیت شبیه وضعیت
+    /// Accumulate
+    /// است
+    /// تنها فرقش این است که نقطه جدیدی نمی شود زد.
+    /// تغییرات لازم را برای این کار بدهید.
+    /// </summary>
+    public class PointState : AccumulateState
+    {
+        public PointState(Calculator calc) : base(calc) { }
+        public override IState EnterPoint()
+        {
+            if (this.Calc.Display == "" || this.Calc.Display == "." || this.Calc.Display == "0.")
+                this.Calc.Display = "0.";
+            else if (!this.Calc.Display.Contains("."))
+                this.Calc.Display += ".";
+            return this;
+        }
+        //#1 لطفا!
+    }
+}
