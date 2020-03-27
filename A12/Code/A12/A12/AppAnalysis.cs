163650200b4eadab27b82692f6aa641748d2a2bb
omidmirzajani <omidmirzajani82@gmail.com>
2020-03-27 16:54:11 +0430

    A12 Added

---
+++ A12/Code/A12/A12/AppAnalysis.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace A12
+{
+    public class AppAnalysis
+    {
+        public List<AppData> Apps=new List<AppData>();
+        public AppAnalysis()
+        { }
+
+        public static AppAnalysis AppAnalysisFactory(string csvAddress)
+        {
+            var appAnalysis = new AppAnalysis();
+            using (TextFieldParser parser = new TextFieldParser(csvAddress))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                var fields = parser.ReadFields();
+                int i = 0;
+                while (!parser.EndOfData)
+                {
+                    i++;
+                    fields = parser.ReadFields();
+                    appAnalysis.AppendApp(fields);
+                }
+            }
+            return appAnalysis;
+        }
+
+        public void AppendApp(string[] fields)
+        {
+            AppData ap = new AppData(fields);
+            this.Apps.Add(ap);
+        }
+
+        public int AllAppsCount()
+        {
+            return this.Apps
+                       .ToArray()
+                       .Length;
+        }
+
+        public object AppsAboveXRatingCount(double v)
+        {
+            return this.Apps
+                       .Where(l => (l.Rating >= v))
+                       .ToList()
+                       .Count;                              
+        }
+
+        public string RecentlyUpdatedCount(DateTime dateTime)
+        {
+            return this.Apps
+                       .Where(l => l.LastUpdate >= dateTime)
+                       .ToArray()
+                       .Length
+                       .ToString();
+        }
+
+        public string RecentlyUpdatedFreqCat(DateTime dateTime)
+        {
+            return this.Apps
+                        .Where(l => l.LastUpdate >= dateTime)
+                        .GroupBy(d => d.Category)
+                        .OrderByDescending(g => g.Count())
+                        .Select(l => l.First().Category)
+                        .First();
+                        
+        }
+        
+        public List<string> MostRatedCategories(double v1, int v2)
+        {            
+            return this.Apps
+                .Where(l => l.Rating > v1)
+                .GroupBy(d => d.Category)
+                .OrderByDescending(g => g.Count())
+                .Select(l => l.First().Category)
+                .Take(v2)
+                .ToList();            
+        }
+
+        public Tuple<string,string> ExtremeMeanUpdateElapse(DateTime dateTime)
+        {
+            var item1 = this.Apps
+                       .GroupBy(d => d.Category)
+                       .OrderByDescending(g => g.Average(line => DateTime.Now.Ticks - line.LastUpdate.Ticks))
+                       .Select(g => g.Key).Last();
+            var item2 = this.Apps
+                       .GroupBy(d => d.Category)
+                       .OrderByDescending(g => g.Average(line => DateTime.Now.Ticks - line.LastUpdate.Ticks))
+                       .Select(g => g.Key).First();
+            return new Tuple<string, string>(item1, item2);
+            
+
+
+        }        
+
+        public double TopQuarterBoundary()
+        {
+            var item = this.Apps
+                           .Where(d => d.Category == "PHOTOGRAPHY")
+                           .OrderByDescending(d => d.Rating)
+                           .Select(d=>d.Rating)
+                           .ToArray();
+            return item[(Convert.ToInt32(item.Length/4))];
+        }
+
+        public List<string> XMostProfitables(int x)
+        {
+            var tmp = this.Apps
+                        .Where(d => d.IsFree == 0)
+                        .OrderByDescending(d => d.Price * d.Installs)
+                        .Select(a => a.Name)
+                        .Take(x)
+                        .ToList();
+            return tmp;
+
+        }
+
+        public List<string> XCoolestApps(int x, Func<AppData, double> criteria)
+        {
+            var tmp = this.Apps                          
+                          .OrderBy(d => criteria(d))
+                          .Select(a => a.Name)
+                          .Take(x)
+                          .ToList();
+            return tmp;
+
+        }
+    }
+}
---
+++ A12/Code/A12/A12/AppData.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace A12
+{
+    public class AppData
+    {
+        public string Name;
+        public string Category;
+        public double Rating;
+        public long Reviews;
+        public double Size;
+        public long Installs;
+        public long IsFree;
+        public double Price;
+        public string ContentRating;
+        public List<string> Genres;
+        public DateTime LastUpdate;
+        public string CurrentVersion;
+        public string AndroidVersion;
+        public AppData(string[] fields)
+        {
+            this.Name = fields[0];
+            this.Category = fields[1];           
+            if (!double.TryParse(fields[2], out Rating))
+                Rating = 0;
+            try
+            {
+                this.Reviews = long.Parse(fields[3]);
+            }
+            catch
+            {
+                this.Reviews = 0;
+            }
+            
+            try
+            {
+                int len = fields[4].Length;
+                this.Size = double.Parse(fields[4].Substring(0, len - 1));
+            }
+            catch
+            {
+                this.Size = 0;
+            }
+
+            try
+            {
+                int lenI = fields[5].Length;
+                this.Installs = long.Parse(fields[5].Substring(0, lenI - 1), NumberStyles.AllowThousands);
+            }
+            catch
+            {
+                this.Installs = 0;
+            }
+            if (fields[6] == "Free")
+                this.IsFree = 1;
+            else
+                this.IsFree = 0;
+            try
+            {
+                this.Price = double.Parse(fields[7].Replace("$",""));                
+            }
+            catch
+            {
+                this.Price = 0;
+            }
+            this.ContentRating = fields[8];
+            this.Genres = new List<string>() { fields[9] };            
+            try
+            {
+                this.LastUpdate = DateTime.Parse(fields[10]);
+            }
+            catch
+            {
+                this.LastUpdate = DateTime.Now;
+            }
+            this.CurrentVersion = fields[11];
+            this.AndroidVersion = fields[12];
+        }
+    }
+}
