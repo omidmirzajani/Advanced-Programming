a2d22e07fef36fb5e72d2c68c912dc2e84572208
omidmirzajani <omidmirzajani82@gmail.com>
2020-03-27 16:52:17 +0430

    A13 Added

---
+++ A13/Code/A13/A13/SingleFileWatcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+
+namespace EventDelegateThread
+{
+    public class SingleFileWatcher: IDisposable
+    {
+        private Action Msg;
+        private FileSystemWatcher watch;
+        public SingleFileWatcher(string fileName)
+        {
+            watch = new FileSystemWatcher(Path.GetDirectoryName(fileName), Path.GetFileName(fileName));
+            watch.Changed += OnChanged;
+            watch.EnableRaisingEvents = true;
+        }
+        private void OnChanged(object MsgTo, FileSystemEventArgs fse)
+        {
+                Msg?.Invoke();
+        }
+        public void Register(Action msg)
+        {
+            this.Msg += msg;
+        }
+        public void Unregister(Action msg)
+        {
+            this.Msg -= msg;
+        }
+        public void Dispose()
+        {
+            this.watch = null;
+        }
+    }
+}
---
+++ A13/Code/A13/A13/SingleReminderTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventDelegateThread
+{
+    public class SingleReminderTask : ISingleReminder
+    {
+        Task ReminderTask = null;
+        private int delay;
+        private string msg;
+        public event Action<string> Reminder;
+        public SingleReminderTask(string msg, int delay)
+        {
+            this.msg = msg;
+            this.delay = delay;
+        }
+        public string Msg
+        {
+            get
+            {
+                return msg;
+            }
+        }
+        public int Delay
+        {
+            get
+            {
+                return delay;
+            }
+        }
+        public void Start()
+        {
+            ReminderTask = new Task(() => Reminder(msg));
+            ReminderTask.Start();
+        }
+    }
+}
